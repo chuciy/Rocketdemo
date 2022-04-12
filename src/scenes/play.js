5a1b75e7717447229289c4b9eb1891ce30510da8
chuciy <czhao48@ucsc.edu>
2022-04-12 03:35:47 -0700

    background and enemy

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -12,17 +12,26 @@ class Play extends Phaser.Scene{
     }
 
     create(){
-       this.add.rectangle(0, 0, game.config.width, game.config.height/10, 0xfacade).setOrigin(0, 0);
-       this.p1Rocket = new Rocket(this, game.config.width/2, 300, 'rocket' );
-       this.ship1 = new Ship(this, game.config.width/3, 100, 'spaceship' );
-
-       keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-       keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.gameOver = false;
+        //UI and background
+        this.starfield = this.add.tileSprite(
+            0,0,640,480, 'starfield'
+        ).setOrigin(0,0);
+        this.add.rectangle(0, 0, game.config.width, game.config.height/10, 0xfacade).setOrigin(0, 0);
+        //initialize objects
+        this.p1Rocket = new Rocket(this, game.config.width/2, 300, 'rocket' );
+        this.ship1 = new Ship(this, game.config.width/3, 100, 'spaceship' );
+        this.ship2 = new Ship(this, game.config.width/2, 275, 'spaceship' );
+        this.ship3 = new Ship(this, game.config.width/1, 425, 'spaceship' );
+        //keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update(){
+        this.starfield.tilePositionX -= 6;
         if(keyLEFT.isDown){
             this.p1Rocket.x -= 4;
         }
@@ -31,6 +40,8 @@ class Play extends Phaser.Scene{
         }
 
         this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
         this.p1Rocket.update();
     }
 }
